f1a7401e36a7d2c3fb604b9f932606625b079491
P.I.M <z55666@yandex.ru>
2022-07-09 00:13:50 +0300

    Homework_1_C#

---
+++ Task_6/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num % 2 == 0) Console.Write("Число делится без остатка!");
+else Console.Write("Число делится с остатком!");
---
+++ Task_8/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int k = 2;
+while(i < num)
+{
+    if(num % 2 == 0)
+    {
+      Console.WriteLine(k);
+      k = k + 2;
+      num = num - 2;
+      
+    }
+    else
+    {
+        num = num - 1;
+        i++;
+    } 
+}
